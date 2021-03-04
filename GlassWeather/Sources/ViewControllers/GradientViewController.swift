6c0d2b40181579d8bc7f17435480e63fd9e68a66
jsryudev <jsryu.dev@gmail.com>
2021-03-04 13:45:32 +0900

    Update sample gradient animation values

--- GlassWeather/Sources/ViewControllers/GradientViewController.swift
+++ GlassWeather/Sources/ViewControllers/GradientViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 import AnimatedGradientView
 import SnapKit
+import Then
 
 class GradientViewController: BaseViewController {
-  private let backgroundView = AnimatedGradientView()
+  private let backgroundView = AnimatedGradientView().then {
+    $0.animationValues = [
+      (colors: ["#2BC0E4", "#EAECC6", "#2BC0E4"], .up, .axial),
+      (colors: ["#2BC0E4", "#EAECC6", "#2BC0E4"], .right, .axial),
+      (colors: ["#2BC0E4", "#EAECC6", "#2BC0E4"], .down, .axial),
+      (colors: ["#2BC0E4", "#EAECC6", "#2BC0E4"], .left, .axial)
+    ]
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
